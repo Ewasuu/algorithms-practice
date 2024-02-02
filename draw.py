fdaf48e9aa5c77aa282ca29fc6c81b2fed2f2007
Edward Colmenarez <edward.colmenarez@betaboxtechnologies.com>
2024-02-02 10:43:15 -0400

    draw

---
+++ draw.py
@@ -0,0 +1,15 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_screen = turtle.Screen
+
+
+def draw(my_turtle, length):
+    if length > 0:
+        my_turtle.forward(length)
+        my_turtle.left(90)
+        draw(my_turtle, length - 10)
+
+
+draw(my_turtle, 100)
+my_screen.exitonclick()
